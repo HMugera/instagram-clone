fd161cd3366a09be207bb6618fc5adddd87e74bb
MugeraH <bwosihughes24@gmail.com>
2021-05-25 15:32:19 +0300

    add tests for profile model class

--- instagram/tests.py
+++ instagram/tests.py
@@ -52,6 +52,7 @@ class CommentsTestClass(TestCase):
         Post.objects.all().delete()
         User.objects.all().delete()
         Comments.objects.all().delete()
+        Profile.objects.all().delete()
         
             
     def test_instance(self):
@@ -67,6 +68,36 @@ class CommentsTestClass(TestCase):
         self.comment.delete_comment()
         comments=Comments.objects.all()
         self.assertTrue(len(comments)==0)
+        
+        
+class ProfileTestClass(TestCase):
+    #setup method
+    def setUp(self):
+        self.user = User(username='Mugera')
+        self.user.save()
+        self.user_profile = Profile(user=self.user,profile_picture="good_picture.png")
+      
+        
+    def tearDown(self):
+        User.objects.all().delete()
+        Profile.objects.all().delete()
+   
+            
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user_profile,Profile))
+            
+    def test_save_user_profile(self):
+        self.user_profile.save_profile()
+        profiles=Profile.objects.all()
+        self.assertTrue(len(profiles)>0)
+        
+    def test_delete_user_profile(self):
+        self.user_profile.save_profile()
+        self.user_profile.delete_profile()
+        profiles=Profile.objects.all()
+        self.assertTrue(len(profiles)==0)
+        
+
 
 
 
